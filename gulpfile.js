51a4e6c35c1345b521b35ae2f65c9cda06181f80
Rebecca Palko <rpalko27@gmail.com>
2020-04-09 15:59:51 -0400

    Set up ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,45 @@
-const gulp = require ('gulp');
-const {series} = require ('gulp');
-const sass = require ('gulp-sass');
+const gulp = require('gulp');
+const {series} = require('gulp');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+const eslint = require('gulp-eslint');
 
 
-function styles (cb) {
+function styles(cb) {
   gulp.src('sass/**/*.scss')
-    .pipe (sass())
-    .on('error', sass.logError)
-    .pipe(
-      autoprefixer({
-        browserlist:["last 2 versions"]
-      })
-    )
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
-    cb();
+      .pipe(sass())
+      .on('error', sass.logError)
+      .pipe(
+          autoprefixer({
+            browserlist: ['last 2 versions'],
+          })
+      )
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
+  cb();
 }
 
-function watch(cb){
+function watch(cb) {
   gulp.watch('sass/**/*.scss', gulp.parallel(styles));
   gulp.watch('sass/**/*.scss') .on('change', reload);
   gulp.watch('*.html').on('change', reload);
   gulp.watch('js/**/*.js').on('change', reload);
+  gulp.watch('js/**/*.js', gulp.series(lint));
   browserSync.init({
-    server: "./"
+    server: './',
   });
   cb();
 }
 
+function lint(cb) {
+  gulp.src(['js/**/*.js'])
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+  cb();
+}
+
 exports.styles = styles;
-exports.default = series(styles, watch);
+exports.default = series(styles, lint, watch);
