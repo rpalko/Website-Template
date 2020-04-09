a7abd046cb559f51636db78cbcc5d1f7b9846beb
Rebecca Palko <rpalko27@gmail.com>
2020-04-09 14:07:46 -0400

    Set up sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 const gulp = require ('gulp');
 const {series} = require ('gulp');
+const sass = require ('gulp-sass');
 
-exports.default = series();
+
+function styles (cb) {
+  gulp.src('sass/**/*.scss')
+    .pipe (sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./css'))
+    cb();
+}
+
+exports.styles = styles;
+exports.default = series(styles);
