d4b9f1aa8938d310c239e13a32a4dd8199345bbd
Rebecca Palko <rpalko27@gmail.com>
2020-04-09 15:05:19 -0400

    Set up live editiing

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,35 @@
 const gulp = require ('gulp');
 const {series} = require ('gulp');
 const sass = require ('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 
 function styles (cb) {
   gulp.src('sass/**/*.scss')
     .pipe (sass())
     .on('error', sass.logError)
+    .pipe(
+      autoprefixer({
+        browserlist:["last 2 versions"]
+      })
+    )
     .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
     cb();
 }
 
+function watch(cb){
+  gulp.watch('sass/**/*.scss', gulp.parallel(styles));
+  gulp.watch('sass/**/*.scss') .on('change', reload);
+  gulp.watch('*.html').on('change', reload);
+  gulp.watch('js/**/*.js').on('change', reload);
+  browserSync.init({
+    server: "./"
+  });
+  cb();
+}
+
 exports.styles = styles;
-exports.default = series(styles);
+exports.default = series(styles, watch);
